01e0f983c5440d78e66cb389fbd188783ed0cc88
satoru-tak <satoru.takaira@gmail.com>
2024-03-10 16:16:33 +0900

    stocksの修正やCRUD機能を追加

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -1,9 +1,9 @@
-class Admin::StocksController < ApplicationController
+class Admin::StocksController < AdminController
   before_action :set_admin_stock, only: %i[ show edit update destroy ]
 
   # GET /admin/stocks or /admin/stocks.json
   def index
-    @admin_stocks = Admin::Stock.all
+    @admin_stocks = Stock.all
   end
 
   # GET /admin/stocks/1 or /admin/stocks/1.json
@@ -12,20 +12,24 @@ class Admin::StocksController < ApplicationController
 
   # GET /admin/stocks/new
   def new
-    @admin_stock = Admin::Stock.new
+    @product = Product.find(params[:product_id])
+    @admin_stock = Stock.new
   end
 
   # GET /admin/stocks/1/edit
   def edit
+    @product = Product.find(params[:product_id])
+    @admin_stock = Stock.find(params[:id])
   end
 
   # POST /admin/stocks or /admin/stocks.json
   def create
-    @admin_stock = Admin::Stock.new(admin_stock_params)
+    @product = Product.find(params[:product_id])
+    @admin_stock = @product.stocks.new(admin_stock_params)
 
     respond_to do |format|
       if @admin_stock.save
-        format.html { redirect_to admin_stock_url(@admin_stock), notice: "Stock was successfully created." }
+        format.html { redirect_to admin_product_stock_url(@product, @admin_stock), notice: "Stock was successfully created." }
         format.json { render :show, status: :created, location: @admin_stock }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +42,7 @@ class Admin::StocksController < ApplicationController
   def update
     respond_to do |format|
       if @admin_stock.update(admin_stock_params)
-        format.html { redirect_to admin_stock_url(@admin_stock), notice: "Stock was successfully updated." }
+        format.html { redirect_to admin_product_stock_url(@admin_stock.product, @admin_stock), notice: "Stock was successfully updated." }
         format.json { render :show, status: :ok, location: @admin_stock }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +56,7 @@ class Admin::StocksController < ApplicationController
     @admin_stock.destroy
 
     respond_to do |format|
-      format.html { redirect_to admin_stocks_url, notice: "Stock was successfully destroyed." }
+      format.html { redirect_to admin_product_stocks_url, notice: "Stock was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -60,11 +64,11 @@ class Admin::StocksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin_stock
-      @admin_stock = Admin::Stock.find(params[:id])
+      @admin_stock = Stock.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def admin_stock_params
-      params.require(:admin_stock).permit(:product_id, :amount, :size)
+      params.require(:stock).permit(:amount, :size)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,15 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_10_053216) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "admin_stocks", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.integer "amount"
-    t.string "size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_admin_stocks_on_product_id"
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,8 +72,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_10_053216) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "stocks", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.integer "amount"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_stocks_on_product_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "admin_stocks", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "stocks", "products"
 end
