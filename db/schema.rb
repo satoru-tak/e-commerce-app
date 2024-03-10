f0857cf8ce1e27c034ccca7b53a194f41b89e036
satoru-tak <satoru.takaira@gmail.com>
2024-03-10 14:40:22 +0900

    add stock models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_21_120855) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_10_053216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_21_120855) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admin_stocks", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.integer "amount"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_admin_stocks_on_product_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -74,5 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_21_120855) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "admin_stocks", "products"
   add_foreign_key "products", "categories"
 end
