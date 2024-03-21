29189c84049d365b40c4871429c0f5cd24c9462c
satoru-tak <satoru.takaira@gmail.com>
2024-03-22 08:52:26 +0900

    stimulus.jsをダウンロード

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,8 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "chart.js" # @4.4.2
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
